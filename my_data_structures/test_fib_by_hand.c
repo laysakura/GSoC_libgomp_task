22013d89780882a477238d32c56578d090ce1aaa
Sho Nakatani <lay.sakura@gmail.com>
2011-06-19 15:59:21 +0900

    Finished writing test_fib_by_hand.c
    But it's too slow...
    
    Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

--- my_data_structures/test_fib_by_hand.c
+++ my_data_structures/test_fib_by_hand.c
@@ -10,7 +10,7 @@
 
 #define OMP_TASK_STACK_SIZE_DEFAULT 0x010000L
 
-#define NUM_THREADS 1
+#define NUM_THREADS 2
 __thread unsigned int _thread_id;
 unsigned int _num_detected_threads = 0;
 pthread_t _pthread_id[NUM_THREADS];
@@ -61,15 +61,14 @@ void gsoc_task_scheduler_loop()
       next_task = gsoc_taskqueue_pop(_workers[_thread_id].taskq);
       if (__builtin_expect(next_task == NULL, 0))
         {
-          /* next_task = gsoc_taskqueue_take(taskq2); */
-          /* if (!next_task) */
-          /*   { */
-          /*     printf("gsoc_task_scheduler_loop: num_team_task %d\n", num_team_task); */
+          next_task = gsoc_taskqueue_take(_workers[(_thread_id + 1) % 2].taskq);
+          if (!next_task)
+            {
               if (num_team_task == 0)
                 return; /* to main */
 
               continue; /* try again */
-            /* } */
+            }
         }
 
       _workers[_thread_id].current_task = next_task;
@@ -154,7 +153,7 @@ int fib(int N)
   return f1 + f2;
 }
 
-void gsoc_setaffinity(unsigned int thread_id)
+void gsoc_setaffinity()
 {
   cpu_set_t cpuset;
   CPU_ZERO(&cpuset);
@@ -166,8 +165,10 @@ void* start_master_thread(omp_internal_data* data)
 {
   gsoc_task* root_task;
 
+  /* Set thread id incrementally */
   _thread_id = __sync_add_and_fetch(&_num_detected_threads, 1) - 1;
-  gsoc_setaffinity(_thread_id);
+
+  gsoc_setaffinity();
 
   _workers[_thread_id].scheduler_task = co_create(gsoc_task_scheduler_loop, NULL, NULL, OMP_TASK_STACK_SIZE_DEFAULT);
   root_task = gsoc_task_create((void(*)(void*))fib_outlined, data, NULL, OMP_TASK_STACK_SIZE_DEFAULT, NULL);
@@ -180,18 +181,34 @@ void* start_master_thread(omp_internal_data* data)
   co_call(_workers[_thread_id].scheduler_task);
 }
 
+void* start_slave_thread()
+{
+  /* Set thread id incrementally */
+  _thread_id = __sync_add_and_fetch(&_num_detected_threads, 1) - 1;
+
+  gsoc_setaffinity();
+
+  _workers[_thread_id].scheduler_task = co_create(gsoc_task_scheduler_loop, NULL, NULL, OMP_TASK_STACK_SIZE_DEFAULT);
+
+  co_vp_init(); /* Necessary to set initial value for "co_curr__" in pcl.c.
+                   Without this, SEGV would happen because
+                   swapcontext(co_curr__->context, co_next->context)
+                   is called in pcl.c internally. */
+  co_call(_workers[_thread_id].scheduler_task);
+}
+
 void gsoc_run_workers(omp_internal_data* data)
 {
-  _workers[_thread_id].taskq = gsoc_taskqueue_new();
-  /* _workers[1].taskq = gsoc_taskqueue_new(); */
+  _workers[0].taskq = gsoc_taskqueue_new();
+  _workers[1].taskq = gsoc_taskqueue_new();
 
   pthread_create(&_pthread_id[0], NULL, (void*(*)(void*))start_master_thread, data);
-  /* pthread_create(&_pthread_id[1], NULL, start_slave_thread, NULL); */
+  pthread_create(&_pthread_id[1], NULL, (void*(*)(void*))start_slave_thread, NULL);
   pthread_join(_pthread_id[0], NULL);
-  /* pthread_join(_pthread_id[1], NULL); */
+  pthread_join(_pthread_id[1], NULL);
 
   gsoc_taskqueue_delete(_workers[0].taskq);
-  /* gsoc_taskqueue_delete(_workers[1].taskq); */
+  gsoc_taskqueue_delete(_workers[1].taskq);
 }
 
 
